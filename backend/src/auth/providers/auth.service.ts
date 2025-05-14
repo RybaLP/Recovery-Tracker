460c6a82a7033fbc94743b1d83d2ea7507b2ece2
RybaLP <boop2@onet.pl>
2025-05-14 14:32:08 +0200

    authservice changes

--- backend/src/auth/providers/auth.service.ts
+++ backend/src/auth/providers/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable, ValidationPipe } from '@nestjs/common';
 import { SignInProvider } from './sign-in.provider';
 import { LoginUserDto } from 'src/user/dto/loginUser.dto';
+import { RefreshTokensProvider } from './refresh-tokens.provider';
+import { RefreshTokenDto } from '../dto/refresh-token.dto';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly signInProvider : SignInProvider){}
+    constructor(private readonly signInProvider : SignInProvider,
+        private readonly refreshTokenProvider : RefreshTokensProvider
+    ){}
 
     public signIn( loginUserDto : LoginUserDto){
         return this.signInProvider.signIn(loginUserDto);
     }
-    
+
+    public async refreshTokens(refreshTokensDto : RefreshTokenDto){
+        return await this.refreshTokenProvider.refreshTokens(refreshTokensDto)
+    }
+
 }
